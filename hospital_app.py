b36e5aa84e4a14c978f731d8f9174910bed966da
Miles <milesmorales394@gmail.com>
2026-07-05 18:43:25 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import os
 
-st.set_page_config(page_title"smart hospltal paient navigator" , page_icon="",
+st.set_page_config(page_title="smart hospltal paient navigator" , page_icon="",
 layout="wide")
 
 st.title("smart hospitel patient navigator")
