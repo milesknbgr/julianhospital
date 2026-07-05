fc1417795972a5ccce76737a267dc505c2765a33
Miles <milesmorales394@gmail.com>
2026-07-05 18:43:03 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
 
+st.set_page_config(page_title"smart hospltal paient navigator" , page_icon="",
+layout="wide")
+
+st.title("smart hospitel patient navigator")
